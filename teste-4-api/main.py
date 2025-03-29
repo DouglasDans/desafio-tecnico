24d2b34c76f8f9f4852241eb7e7803a3091cc74a
douglasdans <douglas.dans@gmail.com>
2025-03-29 12:42:58 -0300

    feat: adiciona frontend ui com vueJS

--- teste-4-api/main.py
+++ teste-4-api/main.py
@@ -1,13 +1,20 @@
 from sqlalchemy import create_engine, text
-
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
 
 engine = create_engine("mysql+pymysql://root:password@0.0.0.0:3306/yourdb")
 
-
-
 app = FastAPI()
+
+# Add CORS middleware to allow all origins
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],  # Allow all origins
+  allow_credentials=True,
+  allow_methods=["*"],  # Allow all methods
+  allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/operadoras")
 def read_root():
@@ -18,4 +25,4 @@ def read_root():
     for row in result:
       operadoras.append(row._asdict())
     
-    return operadoras 
+    return operadoras
---
+++ teste-4-api/vue-ui/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>Registros de operadoras</h1>
+  <div class="card">
+      <DataTable :value="products" tableStyle="min-width: 50rem">
+          <Column field="REGISTRO_OPERADORA" header="REGISTRO_OPERADORA"></Column>
+          <Column field="CNPJ" header="CNPJ"></Column>
+          <Column field="RAZAO_SOCIAL" header="RAZAO_SOCIAL"></Column>
+          <Column field="NOME_FANTASIA" header="NOME_FANTASIA"></Column>
+          <Column field="MODALIDADE" header="MODALIDADE"></Column>
+          <Column field="LOGRADOURO" header="LOGRADOURO"></Column>
+          <Column field="NUMERO" header="NUMERO"></Column>
+          <Column field="COMPLEMENTO" header="COMPLEMENTO"></Column>
+          <Column field="BAIRRO" header="BAIRRO"></Column>
+          <Column field="CIDADE" header="CIDADE"></Column>
+          <Column field="UF" header="UF"></Column>
+          <Column field="CEP" header="CEP"></Column>
+          <Column field="DDD" header="DDD"></Column>
+          <Column field="TELEFONE" header="TELEFONE"></Column>
+          <Column field="FAX" header="FAX"></Column>
+          <Column field="EMAIL" header="EMAIL"></Column>
+          <Column field="REPRESENTANTE" header="REPRESENTANTE"></Column>
+          <Column field="CARGO_REPRESENTANTE" header="CARGO_REPRESENTANTE"></Column>
+          <Column field="REGIAO_COMERCIALIZACAO" header="REGIAO_COMERCIALIZACAO"></Column>
+          <Column field="DATA_REGISTRO_ANS" header="DATA_REGISTRO_ANS"></Column>
+      </DataTable>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+
+const products = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost:8000/operadoras');
+    const data = await response.json();
+    products.value = data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+});
+</script>
